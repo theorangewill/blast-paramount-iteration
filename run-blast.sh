2fad125d3e29f58c6a0ce57a1d399b35e98c77e2
William Felipe da Cunha Tavares <ra230321@brasil.lab.ic.unicamp.br>
2019-06-12 17:11:35 -0300

    Script Run

---
+++ run-blast.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# GERANDO ENTRADAS A PARTIR DE DUAS BASES DE DADO
+echo "GERANDO ENTRADAS"
+cd blast-paramount-iteration/refseq
+ENTRADA0=entrada00.fa
+ENTRADA1=entrada01.fa
+blastdbcmd -db refseq_rna.00 -entry all -out entrada00.fa
+echo "		" $ENTRADA0 " CRIADA"
+blastdbcmd -db refseq_rna.01 -entry all -out entrada01.fa
+echo "		" $ENTRADA1 " CRIADA"
+
+# RODAR OS EXPERIMENTOS
+
+cd ..
+
+NUMEROCORES=2
+NUMEROCORES=$(nproc)
+
+echo "NUMERO DE CORES: " $NUMEROCORES
+
+echo "RODAR ALGORITMO"
+
+for j in {$ENTRADA0,$ENTRADA1}
+do
+echo "	EXPERIMENTO COM " $j
+
+for ((THREADS=2; THREADS <= $NUMEROCORES ; THREADS*=2))
+do
+	echo "		EXPERIMENTO COM " $THREADS " THREADS"
+	echo "*"
+	for i in {1..5}
+	do
+		blastn -query ref_seq/entrada00.fa -db ref_seq/refseq_rna.00 -out saida.out -max_target_seqs 2 -task blastn -num_threads $threads
+	done
+	echo "*"
+done
+
+if [ $NUMEROCORES -ge 48 ]; then
+	echo "		EXPERIMENTO COM " $NUMEROCORES " THREADS"
+	echo "*"
+	for i in {1..5}
+	do
+		blastn -query ref_seq/entrada00.fa -db ref_seq/refseq_rna.00 -out saida.out -max_target_seqs 2 -task blastn -num_threads $threads
+	done
+	echo "*"
+fi
+
+done
