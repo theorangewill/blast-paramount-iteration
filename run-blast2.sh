09385c175d73cc4a4e9bdda7d57f721a88aaea8e
William Felipe <theorangewill@gmail.com>
2019-08-09 13:50:02 -0300

    Update run-blast2.sh

--- run-blast2.sh
+++ run-blast2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # GERANDO DBS A PARTIR DE DUAS SEQUENCIAS
-DBMINI=entradas/human_genoma_mini
-makeblastdb -in $DBMINI.fa -parse_seqids -dbtype nucl -out entradas/human_genoma_mini > saida.out
+DBPEQUENO=entradas/human_genoma_pequeno
+makeblastdb -in $DBPEQUENO.fa -parse_seqids -dbtype nucl -out entradas/human_genoma_pequeno > /dev/null
 ENTRADA=entradas/Homo_sapiens.GRCh38.cds.all.fa
 
 # RODAR OS EXPERIMENTOS
 NUMEROCORES=$(nproc)
-blastn -query $ENTRADA -db $DBMINI -out saida.out -max_target_seqs 5 -task blastn -num_threads $NUMEROCORES
+blastn -query $ENTRADA -db $DBPEQUENO -out /dev/null -max_target_seqs 5 -task blastn -num_threads $NUMEROCORES
 
